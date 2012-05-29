f049da2ace1050afa7916dd5721fb5533f86ecd9
Efremov Alex <efremov@crvtu.local>
2012-05-29 10:22:47 +0400

    create distr

---
+++ setup.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='genericrelationview',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/lexich/genericrelationview',
+    license='BSD',
+    author='lexich',
+    author_email='lexich121@gmail.com',
+    description='admin interface for GenericForeignKey fields'
+)
