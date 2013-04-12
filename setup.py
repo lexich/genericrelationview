d4c686738fcba0570780b55f490702e1e70522e2
Efremov Alexey <efremov@crvtu.local>
2013-04-12 14:57:05 +0400

    update name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='genericrelationview',
+    name='django-genericrelationview',
     version='0.0.1',
     include_package_data=True,
     packages=find_packages(),
